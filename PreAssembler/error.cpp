f30e4f1e15c6c76c4436acaf9f5fbfe163f1db6c
Unknown <finlay@dal.ca>
2018-06-03 11:00:17 -0300

    Split program into various files
    Separating functions and classes into different files for easier debugging

---
+++ PreAssembler/error.cpp
@@ -0,0 +1,21 @@
+/*
+- Pre-assembler for the X-Makina Project
+- ECED3403 Assignment 1
+- This is the source file for the pre-assembler's very basic error handler
+- Finlay Miller B00675696
+- 3 June 2018
+*/
+
+#include "error.h"
+
+void error(string instruction)
+{
+	/*
+	Purpose:    Writes errors to screen
+	Input:      Instruction being examined at time of error
+	Output:     None
+	*/
+
+	cout << "Error found due to record for instruction " << instruction << "\n";
+	return;
+}
---
+++ PreAssembler/error.h
@@ -0,0 +1,20 @@
+/*
+- Pre-assembler for the X-Makina Project
+- ECED3403 Assignment 1
+- This is the header file for the pre-assembler's very basic error handler
+- Finlay Miller B00675696
+- 3 June 2018
+*/
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void error(string instruction);
+
+#endif // !ERROR_H
